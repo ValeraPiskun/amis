100f07073be22d64fd987aeedbb47d098efaba74
N <hik951@gmail.com>
2018-11-21 20:23:23 +0200

    work 3

--- km-84/Peeper_Valery/workshop3/source/main.py
+++ km-84/Peeper_Valery/workshop3/source/main.py
@@ -1,8 +1,57 @@
-"""
-Тут написати умову до завдання
-"""
+dataset = {
+        "bob@gmail.com": {
+                            "human": {
+                                        "name": "bob",
+                                        "age": 19
+                                    },
+                            "food": {
+                                        "sup": {
+                                                "carrot": 2 ,
+                                                "onion": 1 ,
+                                                "water": 3
+                                              },
+                                        "salat": {
+                                                "salat": 2 ,
+                                                "onion": 1,
+                                                "tomat": 3
+                                                }
+                                    }
+                        },
+
+    "boba@gmail.com": {
+        "human": {
+            "name": "boba",
+            "age": 19
+        },
+        "food": {
+            "sup": {
+                "carrot": 2
+
+            }
+        }
+    }
+
+}
+# def getNames(dataset, keys):
+#     if keys==[]:
+#         return
+#     key = keys[0]
+#     name = dataset[key]["human"]["name"]
+#     print(name)
+#     getNames(dataset, keys[1:])
+# getNames(dataset,list(dataset.keys()))
+#
+for key in list(dataset.keys()):
+    humanName = dataset[key]["human"]["name"]
 
 
-
-if __name__ == "__main__":
-    #TODO
+    products = set()
+    for food in dataset[key]["food"].values():
+        for product in food:
+            products.add(product)
+    print(humanName,len(products))
+    productSum = 0
+    for food in dataset[key]["food"].values():
+        for product in food.values():
+            productSum+= product
+    print(humanName,productSum)
